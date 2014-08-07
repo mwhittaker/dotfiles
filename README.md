79d9de75731b2e305cba84d0221420f4367132b7
Michael Whittaker <mjw297@cornell.edu>
2014-08-07 06:34:56 +0000

    Added installation script.
    In commit #587c0a637aa1ef99d38e77a68ac870537daa979a, I deleted a
    Makefile that acted as an installtion script. At the time, I wasn't
    using VM's often and I really never needed to install the dotfiles. Now,
    I deal with VM's a lot more and often need to install dotfiles in a sane
    way, so I added back an installation script. This time, I ditched make
    for plain old shell scripting.

--- README.md
+++ README.md
@@ -4,4 +4,4 @@ My collection of miscellaneous .* files.
 ## Installation ##
 
     git clone
-    make install
+    ./install.sh
---
+++ install.sh
@@ -0,0 +1,43 @@
+#! /bin/sh
+
+link() {
+    ln -s -i "$(pwd)/$1" $2
+}
+
+install_bash_aliases() {
+    link .bash_aliases ~/.bash_aliases 
+}
+
+install_bashrc() {
+    link .bashrc ~/.bashrc
+}
+
+install_gitconfig() {
+    link .gitconfig ~/.gitconfig
+}
+
+install_tmux_conf() {
+    local tmux_version=$(tmux -V | awk '{print $2}')
+    if test $(echo "$tmux_version <= 1.8" | bc) -eq 1; then 
+        link .tmux.conf-1.8 ~/.tmux.conf
+    else
+        link .tmux.conf-1.9 ~/.tmux.conf
+    fi
+}
+
+install_vimrc() {
+    if test ! -d ~/.vim/bundle/Vundle.vim; then
+        git clone https://github.com/gmarik/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+    fi
+    link .vimrc ~/.vimrc
+}
+
+main() {
+    install_bash_aliases
+    install_bashrc
+    install_gitconfig
+    install_tmux_conf
+    install_vimrc
+}
+
+main
